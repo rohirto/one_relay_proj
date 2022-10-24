6d23ee9977d5949941b880f25a310609b6c9badc
rohirto <rohirto@gmail.com>
2022-10-25 03:21:29 +0530

    New features

--- u_macros.h
+++ u_macros.h
@@ -3,9 +3,11 @@
 
 #define MQTT_CLIENT_NAME    "shrirampur_cli_1"
 #define RELAY_TOPIC         "/v1.6/devices/home-automation/relay-control/lv"
-#define TOKEN               "/*rohirto 1 -> home automation device token*/"
+#define TOKEN               "B_B_F_F-wVdGoyx0wgM2LbvIbwhH6Bzo9lnfg6"
 
 #define RELAY_CONTROL_LABEL "relay-control"
 
+//#define OPTO_COUPLER
+#define GPIO_16             16
 
 #endif
